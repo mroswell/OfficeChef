305249e1880a2fd7c146dd477ccaf9e83e14f005
Marjorie Roswell <mroswell@gmail.com>
2018-12-14 07:33:11 -0800

    add vuetifier-loader

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import Vuetify from 'vuetify'
+import Vuetify from 'vuetify/lib'
+
 import { VueMasonryPlugin } from 'vue-masonry'
 import 'vuetify/dist/vuetify.min.css'
 
