ef7157e63ed9a44009fa266eb4bb3f9982507895
Dylan Broussard <deraw@outlook.fr>
2018-12-14 20:37:23 +0100

    ➕ Make it a PWA

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import '@babel/polyfill'
+import './registerServiceWorker'
 
 import Vuetify from 'vuetify/lib'
 import { VueMasonryPlugin } from 'vue-masonry'
