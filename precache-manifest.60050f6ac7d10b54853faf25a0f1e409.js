74b054adccd04df5d1bf9696c16897f32867f5c2
Marjorie Roswell <mroswell@gmail.com>
2019-01-05 21:57:23 -0500

    🚀 Deploy

---
+++ precache-manifest.60050f6ac7d10b54853faf25a0f1e409.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc17e8f555cfdaa367d5",
+    "url": "/OfficeChef/css/chunk-vendors~fdc6512a.5b2abd25.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/OfficeChef/robots.txt"
+  },
+  {
+    "revision": "dc17e8f555cfdaa367d5",
+    "url": "/OfficeChef/js/chunk-vendors~fdc6512a.240efeff.js"
+  },
+  {
+    "revision": "8d87bd5f62fb7a4d393d",
+    "url": "/OfficeChef/js/chunk-vendors~03631906.01cf40f5.js"
+  },
+  {
+    "revision": "ba46245a768116ded6f2",
+    "url": "/OfficeChef/js/chunk-vendors~678f84af.2b23afd7.js"
+  },
+  {
+    "revision": "b56b353f39eecca3795a",
+    "url": "/OfficeChef/js/chunk-vendors~7f98709b.b365c9c0.js"
+  },
+  {
+    "revision": "7a60bcc2980d1708e27a",
+    "url": "/OfficeChef/js/chunk-vendors~b1f96ece.028928b0.js"
+  },
+  {
+    "revision": "f6609722bbd600fa3739",
+    "url": "/OfficeChef/js/chunk-vendors~d2305125.9bbe6b19.js"
+  },
+  {
+    "revision": "4e6274dff5ad87820252",
+    "url": "/OfficeChef/js/chunk-vendors~d939e436.dbd687a6.js"
+  },
+  {
+    "revision": "fcfc7c03a9dfb5faddfd",
+    "url": "/OfficeChef/js/equipment~dde583c9.b7f8236e.js"
+  },
+  {
+    "revision": "ea5a975783f9e28144df",
+    "url": "/OfficeChef/js/chunk-vendors~dabdd5e1.04b80a94.js"
+  },
+  {
+    "revision": "eb4c8db056ca1b78fbd8",
+    "url": "/OfficeChef/js/equipment~a97bfcba.54183fbc.js"
+  },
+  {
+    "revision": "3240fc32c7727d5b1ce8",
+    "url": "/OfficeChef/js/chunk-vendors~203e0718.a92cdec7.js"
+  },
+  {
+    "revision": "43444a75929e390e53e6",
+    "url": "/OfficeChef/js/equipment~4b028b5f.18d43342.js"
+  },
+  {
+    "revision": "1f0a496ccbee105aa80e",
+    "url": "/OfficeChef/js/equipment~5c956a7a.2ec43072.js"
+  },
+  {
+    "revision": "77274ab329e3d111bbed",
+    "url": "/OfficeChef/js/equipment~78934547.cbb8cc77.js"
+  },
+  {
+    "revision": "5be5a03a2af2a6ae111a",
+    "url": "/OfficeChef/js/equipment~a3c98e4e.b1c63453.js"
+  },
+  {
+    "revision": "618ade0575d58cef3a3f",
+    "url": "/OfficeChef/js/app~d0ae3f07.caf3d660.js"
+  },
+  {
+    "revision": "62ffb18e683905f0af930a139a20265f",
+    "url": "/OfficeChef/index.html"
+  },
+  {
+    "revision": "fcfc7c03a9dfb5faddfd",
+    "url": "/OfficeChef/css/equipment~dde583c9.6d9acdc9.css"
+  },
+  {
+    "revision": "eb4c8db056ca1b78fbd8",
+    "url": "/OfficeChef/css/equipment~a97bfcba.46d2d392.css"
+  },
+  {
+    "revision": "ea5a975783f9e28144df",
+    "url": "/OfficeChef/css/chunk-vendors~dabdd5e1.f7fd1f7d.css"
+  },
+  {
+    "revision": "618ade0575d58cef3a3f",
+    "url": "/OfficeChef/css/app~d0ae3f07.230b0abe.css"
+  }
+];
